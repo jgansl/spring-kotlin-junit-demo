72b2c504431312932eabdf75db666fee1ed6c7ed
jesse gansler <jessegansler@gmail.com>
2021-12-04 11:47:00 -0800

    setup

---
+++ src/main/kotlin/com/example/springkotlinjunitdemo/SpringKotlinJunitDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotlinjunitdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinJunitDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinJunitDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springkotlinjunitdemo/SpringKotlinJunitDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springkotlinjunitdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinJunitDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
